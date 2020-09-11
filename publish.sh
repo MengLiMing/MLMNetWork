573ea65df1aedbf1077de423ec2ad99dccd0536e
孟利明 <920459250@qq.com>
2020-09-11 09:06:29 +0800

    update sh

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
-pod repo push MLMSpecs MLMNetWork.podspec --allow-warnings
-pod repo push master MLMNetWork.podspec --allow-warnings
+pod repo push MLMSpecs MLMNetWork.podspec --verbose  --allow-warnings
+pod repo push master MLMNetWork.podspec --verbose --allow-warnings
 pod trunk push MLMNetWork.podspec --verbose --allow-warnings
